dfb13f574695704fe53bf2437de4f03ecca67bd9
junfengqiao <jfqemma@gmail.com>
2024-02-20 19:07:03 -0500

    first commit

---
+++ models/user.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const {Schema, model} = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const userSchema = new Schema({
+    name: {type: String, required:true},
+    email:{type: String, required:true, unique: true},
+    password: {type:String, required:true},
+    blogs: [{type: mongoose.Schema.Types.ObjectId, ref: 'Blog'}]
+}, {
+    timestamps: true
+})
+
+
+
+userSchema.pre('save', async function(next) {
+    this.isModified('password')?
+    this.password = await bcrypt.hash(this.password, 8):
+    null;
+    next()
+})
+
+userSchema.methods.generateAuthToken = async function() {
+    const token = await jwt.sign({_id: this._id}, process.env.SECRET)
+    return token
+}
+
+const User = model('User', userSchema)
+module.exports = User
